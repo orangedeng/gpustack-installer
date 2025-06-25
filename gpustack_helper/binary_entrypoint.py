9c0df61b5b5a319ca744ae6b4e2f483c12b2de88
Yuxing Deng <jxfa0043379@hotmail.com>
2025-06-25 10:19:28 +0800

    test

--- gpustack_helper/binary_entrypoint.py
+++ gpustack_helper/binary_entrypoint.py
@@ -1,10 +1,18 @@
+if __name__ == "__main__":
+    import certifi
+    import os
+
+    if os.getenv("SSL_CERT_FILE", None) is None:
+        os.environ["SSL_CERT_FILE"] = certifi.where()
+        print("ssl cert file set")
+
 from gpustack.main import main as gpustack
 from vox_box.main import main as vox_box
 import multiprocessing
 import re
 import sys
 import os
-import certifi
+import ssl
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
@@ -12,8 +20,8 @@ if __name__ == "__main__":
     binary_name = os.path.basename(
         sys.argv[0]
     )  # Ensure the script name is set correctly
-    if os.getenv("SSL_CERT_FILE", None) is None:
-        os.environ["SSL_CERT_FILE"] = certifi.where()
+    print(ssl.get_default_verify_paths())
+    print(os.getenv("SSL_CERT_FILE", None))
     if binary_name == "vox-box":
         sys.exit(vox_box())
     else:
